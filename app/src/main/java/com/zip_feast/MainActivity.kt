820da1505ffc5d025c02fbcfd08b93ff7f7f3b0a
Yasiralam <yasiralam981@gmail.com>
2024-05-18 16:17:44 +0530

    feature:[Registration#2] added RegistrationScreen and all validations

--- app/src/main/java/com/zip_feast/MainActivity.kt
+++ app/src/main/java/com/zip_feast/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.zip_feast.screens.LoginScreen
+import com.zip_feast.screens.RegistrationScreen
 import com.zip_feast.ui.theme.ZipFeastTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +14,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ZipFeastTheme {
-                LoginScreen()
+//                LoginScreen()
+                RegistrationScreen()
             }
         }
     }
