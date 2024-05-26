3d0eb1b5d5c14d7b904e225646623a323a220013
Yasiralam <yasiralam981@gmail.com>
2024-05-26 10:58:04 +0530

    ✨Feature: added topAppBar

--- app/src/main/java/com/zip_feast/presentation/dashboard/HomeScreen.kt
+++ app/src/main/java/com/zip_feast/presentation/dashboard/HomeScreen.kt
@@ -4,83 +4,113 @@ package com.zip_feast.presentation.dashboard
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-@Preview(showBackground = true)
 @Composable
-private fun PreviewHome() {
-    HomeScreen()
-}
-@Composable
-fun HomeScreen(modifier: Modifier = Modifier) {
-    Box {
+fun HomeScreen() {
+    Box() {
         Scaffold(
             topBar = { topAppBar() },
-            containerColor = MaterialTheme.colorScheme.primary,
+            containerColor = Color.White,
         ) { paddingValues ->
             Content(paddingValues)
-
         }
     }
 }
 
 @Composable
 fun Content(paddingValues: PaddingValues) {
+    Column(Modifier.padding(paddingValues)) {
+        Header()
+    }
+}
+
+@Composable
+fun Header(modifier: Modifier = Modifier) {
 
 }
 
 @Composable
 fun topAppBar() {
-    val searchText = remember {
-        mutableStateOf("")
-    }
+    var searchText by rememberSaveable { mutableStateOf("") }
+
     Row(
         modifier = Modifier
-            .padding(16.dp)
-            .height(48.dp),
+            .padding(top = 50.dp, start = 10.dp, end = 10.dp)
+            .height(60.dp)
+            .fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         TextField(
-            value = searchText.value,
-            onValueChange = { newValue -> searchText.value = newValue },
-            label = { Text(text = "Search Food, grocery etc.", fontSize = 12.sp) },
+            value = searchText,
+            onValueChange = { searchText = it },
+            placeholder = {
+                Text(text = "Search Food, grocery etc.", fontSize = 12.sp)
+            },
             singleLine = true,
             leadingIcon = {
                 Icon(imageVector = Icons.Default.Search, contentDescription = null)
             },
-            colors = TextFieldDefaults.colors(
-                focusedContainerColor = Color.White,
-                unfocusedContainerColor = Color.White,
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = MaterialTheme.colorScheme.surfaceVariant,
                 focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
             ),
             shape = RoundedCornerShape(8.dp),
             modifier = Modifier
                 .weight(1f)
+                .fillMaxHeight()
         )
+        Spacer(modifier = Modifier.width(8.dp))
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(
+                imageVector = Icons.Outlined.FavoriteBorder,
+                contentDescription = "Favorite",
+                tint = Color.Black
+            )
+        }
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(
+                imageVector = Icons.Outlined.Notifications,
+                contentDescription = "Notifications",
+                tint = Color.Black
+            )
+        }
     }
-
 }
+
+
+
