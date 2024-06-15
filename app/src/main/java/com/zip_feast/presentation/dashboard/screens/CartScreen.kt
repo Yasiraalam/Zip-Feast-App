78e49a54770120a35cf2cce6d8a0c41bc05703d0
Yasiralam <yasiralam981@gmail.com>
2024-06-15 23:51:27 +0530

    Added card in cart item list

--- app/src/main/java/com/zip_feast/presentation/dashboard/screens/CartScreen.kt
+++ app/src/main/java/com/zip_feast/presentation/dashboard/screens/CartScreen.kt
@@ -1,24 +1,161 @@
 package com.zip_feast.presentation.dashboard.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.zip_feast.R
+import com.zip_feast.presentation.theme.SkyBlue
+
 
 @Composable
 fun CartScreen() {
-   Box (
-       modifier = Modifier.fillMaxSize(),
-       contentAlignment = Alignment.Center
-   ){
-       Text(
-           text = "CartScreen",
-           color = Color.Blue
-       )
-   }
+//    val cartItems by cartViewModel.cartItems.collectAsState()
+    Scaffold(
+        modifier = Modifier.fillMaxWidth(),
+        topBar = {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(80.dp)
+                    .padding(top = 40.dp),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                Text(
+                    text = "Your Cart",
+                    fontSize = MaterialTheme.typography.titleMedium.fontSize,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(start = 16.dp),
+                    color = Color.Black
+                )
+                IconButton(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .align(Alignment.CenterEnd)
+                        .padding(end = 12.dp),
+                ) {
+                    Icon(
+                        imageVector = Icons.Outlined.Notifications,
+                        contentDescription = "Notifications",
+                        tint = Color.Gray
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+        CartItemsSections(innerPadding)
+    }
+}
 
+@Composable
+fun CartItemsSections(
+    innerPadding: PaddingValues
+) {
+ val CartItems = 3
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding)
+    ) {
+        items(CartItems) { cartItem ->
+            CartItemCard(cartItem)
+        }
+    }
+}
+@Composable
+fun CartItemCard(cartItem: Int) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(130.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .padding(12.dp)
+            .background(Color.White),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        CartItem()
+    }
+}
+@Composable
+private fun CartItem() {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.n1),
+            modifier = Modifier.size(60.dp),
+            contentDescription = "item"
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            Text(
+                text = "Item Name",
+                color = Color.Black,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+            Text(
+                text = "MRP 1200",
+                color = SkyBlue,
+                fontSize = 12.sp,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Outlined.Favorite,
+                    contentDescription = "Favorite",
+                    tint = Color.Red
+                )
+            }
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Outlined.Delete,
+                    contentDescription = "Delete",
+                    tint = Color.Black  // Assuming SkyBlue is a custom color
+                )
+            }
+        }
+    }
 }
