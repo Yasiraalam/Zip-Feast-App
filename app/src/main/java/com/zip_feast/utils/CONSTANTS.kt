4854bfc55f9995e288531baad098a425d7b84bd2
Yasir Aalam <94714966+Yasiraalam@users.noreply.github.com>
2024-08-10 12:33:24 +0530

    Update CONSTANTS.kt

--- app/src/main/java/com/zip_feast/utils/CONSTANTS.kt
+++ app/src/main/java/com/zip_feast/utils/CONSTANTS.kt
@@ -1,6 +1,5 @@
 package com.zip_feast.utils
 
 object CONSTANTS{
-//    const val BASE_URL ="http://192.168.1.32:4000/api/v1/"   //wifi
-    const val BASE_URL ="http://192.168.212.223:4000/api/v1/"  //mobile
+    const val BASE_URL ="base_url" 
 }
